cd5613b2283e99e83883a5b72c60b615d6bdf18a
MikeWeDev <155253264+MikeWeDev@users.noreply.github.com>
2024-02-24 09:44:59 -0300

    added version of HashRouter

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,5 +1,5 @@
 import './index.css'
-import { BrowserRouter as Router, Routes ,Route } from "react-router-dom"
+import { HashRouter as Router, Routes ,Route } from "react-router-dom"
 import Products from './AddtoCart/productPage'
 import Rigister from './pages/Rigister'
 import Login from './pages/Login'
